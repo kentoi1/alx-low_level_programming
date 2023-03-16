ba6554931279ff6dd69d0ee85f6ed7acff4ee804
kentoi1 <odongokentoi@gmail.com>
2023-03-16 11:15:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print proper grammar
+ * return : this return 0 (succuss)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
