821703c3b26aa9fe0b8c51f8f1ab2278c52906dc
kentoi1 <odongokentoi@gmail.com>
2023-03-16 10:46:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - describes programming language
- * return : this return 0 (success)
+ * main - this describes programming
+ * return : this returns 0 (success)
 */
-		int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
