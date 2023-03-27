88000a8ee69e0b96c65722563ebaef9fbf59c89f
kentoi1 <odongokentoi@gmail.com>
2023-03-27 03:50:01 -0700

    main.h

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -6,15 +6,9 @@
  * description: function that takes a pointer to an int as parameter and updates the value
  * return: always 0 (success)
 */
-int main(void)
+void reset_to_98(int *n);
 {
-	int n;
-	int *p;
-
-	n = 98;
-	p = &n;
-	putchar('Address of n: %p\n', &n);
-	putchar('value of p: %p\n', p);
-	return (0)
+	*n = 98;
 }
+
 
