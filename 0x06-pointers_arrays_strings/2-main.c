76d91fecabbd6205749f18a90c7b51df702aff21
kentoi1 <odongokentoi@gmail.com>
2023-03-31 01:36:45 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -13,10 +13,10 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 98 - 1; i++)
-		(
+	{
 		 s1[i] = '*';
-		 }
-	si[i] = '\0';
+	}
+	s1[i] = '\0';
 	printf("%s\n", s1);
 	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
 	printf("%s\n", s1);
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strncpy - function that copies a string
+ * @dest: Input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
